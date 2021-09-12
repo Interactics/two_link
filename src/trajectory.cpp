523b92c7ca3ca541d89c5f3e0514122505d76dc2
interactics <zz31@naver.com>
2021-09-12 18:17:50 +0900

    Added trajoidal traj gen

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -12,6 +12,10 @@ std::vector<double> PointToPoint(const std::vector<double> domain, const double 
 // Cubic Poly
 double cubicPolynomial(double a3, double a2, double a1, double a0, double x);
 
+// Trapezodial
+double qt(double q_i, double q_f, double ddq_c, double t_f, double t); // Pos
+std::vector<double> trapezoidalVelocityProfile(double q_i, double q_f, double ddq_c, double t_f, const std::vector<double> domain);
+double sgn_func(double x);
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "traj_gen");
@@ -29,9 +33,14 @@ int main(int argc, char **argv){
 
     double dq_i = 0;
     double dq_f = 0;
+    double ddq_c = 6 * M_PI; // 선정해야하지만, 임의로 선택.
 
     std::vector<double> t = createDomain(t_i, t_f, timeStep);                // 정의역
-    std::vector<double> q = PointToPoint(t, q_i, q_f, dq_i, dq_f, t_i, t_f); // 치역
+    // Cubic
+	std::vector<double> q = PointToPoint(t, q_i, q_f, dq_i, dq_f, t_i, t_f); // 치역
+
+	// Trapezoidal
+    // std::vector<double> q = trapezoidalVelocityProfile(q_i, q_f, ddq_c, t_f, t);
 
 	std_msgs::Float64 msg;
 
@@ -90,4 +99,57 @@ double cubicPolynomial(
     double a3, double a2, double a1, double a0, double x)
 {
     return a3 * pow(x, 3) + a2 * pow(x, 2) + a1 * x + a0;
+}
+
+
+double sgn_func(double x)
+{
+    if (x > 0)
+    {
+        return 1;
+    }
+    else if (x < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+std::vector<double> trapezoidalVelocityProfile(
+    double q_i, double q_f,
+    double ddq_c,
+    double t_f, const std::vector<double> domain)
+{
+    std::vector<double> range; // 치역
+
+    for (int i = 0; i < domain.size(); ++i)
+    {
+        range.push_back(qt(q_i, q_f, ddq_c, t_f, domain[i]));
+    }
+    return range;
+}
+
+double qt(
+    double q_i, double q_f,
+    double ddq_c,
+    double t_f, double t)
+{
+    double t_c = t_f / 2.0 - 1.0 / 2.0 * sqrt((pow(t_f, 2) * ddq_c - 4.0 * (q_f - q_i)) / ddq_c);
+    double q;
+    if (0 <= t and t < t_c)
+    {
+        q = q_i + 1.0 / 2.0 * ddq_c * pow(t, 2);
+    }
+    else if (t_c < t and t <= t_f - t_c)
+    {
+        q = q_i + ddq_c * t_c * (t - t_c / 2.0);
+    }
+    else if (t_f - t_c < t and t <= t_f)
+    {
+        q = q_f - 1.0 / 2.0 * ddq_c * pow((t_f - t), 2);
+    }
+    return q;
 }
